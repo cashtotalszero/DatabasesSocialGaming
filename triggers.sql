36172644bd332e1945e1a9322fd31408719c3973
Alex <paznoz@gmail.com>
2014-04-22 15:03:06 +0100

    Question 2 trigger added

--- triggers.sql
+++ triggers.sql
@@ -1,54 +1,24 @@
-
-
 /*
---THIS WORKS
-CREATE TRIGGER q2insert 
-AFTER INSERT ON UserToGame 
-FOR EACH ROW
+QUESTION 2: Automatically update a game’s average rating whenever a user adds 
+or updates their rating for that game 
+*/
+
+/* Trigger to change average on INSERT */
+CREATE TRIGGER AvgOnInsert AFTER 
+INSERT ON UserToGame
+FOR EACH ROW 
 	UPDATE Game
-	SET AverageRating=99.0
-	WHERE GameID=NEW.GameID;
-*/
+		SET AverageRating = (SELECT AVG(UserRating) from UserToGame 
+		where UserToGame.GameID=Game.GameID
+		and UserToGame.GameID=NEW.GameID)
+	WHERE Game.GameID=NEW.GameID;
 
-CREATE TRIGGER q2insert 
-AFTER INSERT ON UserToGame 
-FOR EACH ROW
+/* Trigger to change average on UPDATE */
+CREATE TRIGGER AvgOnUpdate AFTER 
+UPDATE ON UserToGame
+FOR EACH ROW 
 	UPDATE Game
-	SET AverageRating=(
-		SELECT AVG(UserRating)FROM Game,UserToGame,UserPublic
-		WHERE 
-			UserPublic.UserName=UserToGame.UserName AND 
-			Game.GameID=UserToGame.GameID AND
-			Game.GameID=NEW.GameID)
-	WHERE GameID=NEW.GameID;
-
-
-/*
-SET AverageRating=(
-		SELECT AVG(Rating)
-		FROM Game,UserPublic,UserToGame
-		WHERE 
-			UserPublic.UserName=NEW.UserName AND 
-			UserPublic.UserName=UserToGame.UserName AND 
-			Game.GameID=UserToGame.GameID)
-*/
-
-/*	
-
-INSERT INTO UserToGame(UserName,GameID)
-	VALUES('AlexParrott',3
-);
-*/
-
-/*
-BELOW WORKS:
-
-SELECT AVG(UserRating)FROM Game,UserToGame,UserPublic
-WHERE 
-	UserPublic.UserName=UserToGame.UserName AND 
-	Game.GameID=UserToGame.GameID AND
-	Game.GameId=4;
-*/
-
-INSERT INTO UserToGame(UserName,GameID,UserRating)
-	VALUES('AlexParrott',4,5);	
+		SET AverageRating = (SELECT AVG(UserRating) from UserToGame 
+		where UserToGame.GameID=Game.GameID
+		and UserToGame.GameID=NEW.GameID)
+	WHERE Game.GameID=NEW.GameID;
