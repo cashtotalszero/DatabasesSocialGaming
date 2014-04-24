3785631f049914489dc713aa3f066514313827b9
William-Woodhead <w.woodhead@hotmail.com>
2014-04-24 13:13:42 +0100

    drop tables sorted

--- WorkingSQL/dropTablesAll.sql
+++ WorkingSQL/dropTablesAll.sql
@@ -1,15 +1,22 @@
 --Drop all tables in correct order
+
+DROP TABLE AchievementToUserToGame;
+DROP TABLE Achievement;
+DROP TABLE Hotlist;
 DROP TABLE LeaderboardToUserToGame;
 DROP TABLE Plays;
 DROP TABLE Leaderboard;
 DROP TABLE UserToGame;
+DROP TABLE GameImage;
 DROP TABLE GameToGenre;
 DROP TABLE Genre;
-DROP TABLE AchievementToUserToGame;
-DROP TABLE Achievemnent;
-DROP TABLE GameImage;
 DROP TABLE Game;
-DROP TABLE Friends;
 DROP TABLE Friends2;
+DROP TABLE Friends;
 DROP TABLE UserPrivate;
 DROP TABLE UserPublic;
+
+
+
+
+
