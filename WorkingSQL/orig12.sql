bc15924f81b73fd223723c18f7dceb9cca63cee2
Alex <paznoz@gmail.com>
2014-05-02 14:36:01 +0100

    Amended q12

---
+++ WorkingSQL/orig12.sql
@@ -0,0 +1,51 @@
+DROP PROCEDURE IF EXISTS ShowFriends;
+DELIMITER $$
+CREATE PROCEDURE ShowFriends(IN User VARCHAR(20))
+BEGIN
+	/* Create a table of all specified user's friends */
+	CREATE TABLE AllFriends(
+		SELECT Friend FROM(
+			(SELECT * FROM Friends) 
+			UNION DISTINCT 
+			(SELECT * FROM Friends2)) 
+		AS CombinedFriends 
+		WHERE AccHolder = User
+	);
+	/* Create a table of last games played by each friend */
+	/* (1) Get the date of the last play */
+	CREATE TABLE LastDate(	
+		SELECT UserName,MAX(LastPlayDate) AS LastPlay 
+		FROM UserToGame
+		GROUP BY UserName
+		ORDER BY LastPlayDate DESC
+	);
+	/* (2) Get the unique ID and name of the game played on this date */
+	CREATE TABLE LastGame(
+		SELECT UserToGame.UserName,Game.GameID,Name
+		FROM UserToGame
+		JOIN LastDate ON LastDate.UserName = UserToGame.UserName
+		JOIN Game ON UserToGame.GameID = Game.GameID
+		WHERE LastPlay = LastPlayDate
+	);
+
+	/* Display list of all online friends */
+	SELECT UserName,AccountStatus 
+	FROM UserPublic,AllFriends
+	WHERE UserPublic.UserName = AllFriends.Friend
+	AND AccountStatus = 'Online';
+
+	/* Display list of offline friends with last login and last game played */
+	SELECT UserPublic.UserName,AccountStatus,LastLogin,Name AS LastPlayed
+	FROM UserPublic,AllFriends,LastGame
+	WHERE UserPublic.UserName = AllFriends.Friend
+	AND UserPublic.UserName = LastGame.UserName
+	AND AccountStatus = 'Offline';
+
+	DROP TABLE AllFriends;
+	DROP TABLE LastGame;
+	DROP TABLE LastDate;
+
+END; $$
+DELIMITER ;
+
+CALL ShowFriends('AlexParrott');
