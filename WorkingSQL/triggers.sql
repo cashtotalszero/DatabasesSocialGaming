549dd9cab18bdeb7530dce95f322a500d726ddcb
William-Woodhead <w.woodhead@hotmail.com>
2014-05-02 17:24:44 +0100

    question 7 and 11 tidied up

--- WorkingSQL/triggers.sql
+++ WorkingSQL/triggers.sql
@@ -217,31 +217,6 @@ See question7.sql for an example query.
 Author: Will Woodhead
 
 */
-DROP PROCEDURE IF EXISTS DayAndWeekLeaders;
-DELIMITER $$
-CREATE PROCEDURE DayAndWeekLeaders()
-BEGIN 
-
-	SET @gameCount = (SELECT COUNT(GameID) FROM Game);
-	SET @index = 1;
-
-	WHILE( @index <= @gameCount) 
-	DO
-		INSERT INTO Leaderboard (GameID, SortOrder, TimePeriod)
-		VALUES (
-			@index, 
-			(SELECT SortOrder FROM Game WHERE GameID = @index),'1_week'
-		);
-		INSERT INTO Leaderboard (GameID, SortOrder, TimePeriod)
-		VALUES (
-			@index, 
-			(SELECT SortOrder FROM Game WHERE GameID = @index),'1_day'
-		);
-		SET @index = @index + 1;
-	END WHILE ;
-
-END; $$
-DELIMITER ;
 
 /* JAMES THIS PROCEDURE DOES NOT WORK AT PRESENT 
 linked trigger below has also been commented out for this reason
@@ -500,7 +475,9 @@ BEGIN
 			SELECT Friend 
 			FROM Friends
 			AS friendtemp 
-			WHERE AccHolder = UserN)
+			WHERE AccHolder = UserN 
+			UNION
+			SELECT UserN)
 		); 
 
 	IF ((SELECT SortOrder FROM Game WHERE GameID=GID) = 'asc') 
@@ -926,13 +903,24 @@ BEGIN
 	/* Create a default leaderboard at the creation of any new game */
 	INSERT INTO Leaderboard (GameID, IsDefault, SortOrder)
 	VALUES (
-		(SELECT GameID 
-		FROM Game 
-		WHERE Game.GameID = NEW.GameID), 1, (
-			SELECT SortOrder 
+		NEW.GameID,
+		1,
+		(SELECT SortOrder 
 			FROM Game 
 			WHERE Game.GameID = NEW.GameID)
 	);
+	INSERT INTO Leaderboard (GameID, SortOrder, TimePeriod)
+		VALUES (
+			NEW.GameID, 
+			(SELECT SortOrder FROM Game WHERE Game.GameID = NEW.GameID),
+			'1_week'
+		);
+		INSERT INTO Leaderboard (GameID, SortOrder, TimePeriod)
+		VALUES (
+			NEW.GameID, 
+			(SELECT SortOrder FROM Game WHERE Game.GameID = NEW.GameID),
+			'1_day'
+		);
 END $$
 DELIMITER ;
 
