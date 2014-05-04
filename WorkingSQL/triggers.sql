2dacbfd4e5612ce5face69a62951fd0b6ab56513
jamham <jh12021@my.bristol.ac.uk>
2014-05-04 18:31:23 +0100

    q16 added, duplicate q8 trigger removed

--- WorkingSQL/triggers.sql
+++ WorkingSQL/triggers.sql
@@ -221,7 +221,7 @@ A return of true indicates an illegal username, and false if it's acceptable.
 This function is called by a trigger:
 - Before INSERT on UserPublic (which holds user names)
 
-See question8.sql for an exmple query.
+See question8.sql for an example query.
 
 Author: James Hamblion
 */
@@ -710,7 +710,7 @@ BEGIN
 		SELECT * FROM 
 			(SELECT * 
 			FROM Achievement x 
-			WHERE x.gameid = 3) a 
+			WHERE x.gameid = gameident) a 
 			LEFT OUTER JOIN
 			(SELECT * 
 			FROM AchievementToUserToGame y 
@@ -761,6 +761,146 @@ BEGIN
 	DROP TABLE GameAchievementList;
 END; $$
 DELIMITER ;
+
+/* 
+QUESTION 16:
+
+List the games a user and their friend own, their achievement points per game.
+At end of list add all games + achievement points owned by each user that the other does not own.
+
+See question16.sql for example query and testing.
+
+Author: James Hamblion
+*/
+DROP PROCEDURE IF EXISTS CompListGameAchievFriend;
+DELIMITER //
+CREATE PROCEDURE CompListGameAchievFriend(usrname VARCHAR(50), frndusrname VARCHAR(50))
+BEGIN
+	DECLARE ttl VARCHAR(30);
+	DECLARE usrA VARCHAR(20);
+	DECLARE usrpointsA INT;
+	DECLARE usrB VARCHAR(20);
+	DECLARE usrpointsB INT;
+	DECLARE done INT DEFAULT FALSE;
+	/* Cursor for games and achiev of usrname */
+	DECLARE cur CURSOR FOR
+	/* Cursor query start */
+		SELECT query1.GameTitle, User_A, User_A_Points, User_B, User_B_Points
+		FROM
+				(SELECT ID, User_A, GameTitle, SUM(PointValue) AS User_A_Points	--query1
+				 FROM
+					(SELECT ID, User_A, GameTitle, achievementID 
+					 FROM
+						(SELECT ID, UserName AS User_A, name As GameTitle
+						FROM UserToGame u, Game g
+						WHERE u.UserName = usrname AND g.gameid = u.gameid) ug
+					 LEFT OUTER JOIN
+						AchievementToUserToGame atug
+					 ON ug.ID = atug.userToGameID) x
+				  LEFT OUTER JOIN 
+					Achievement y
+				 ON x.achievementID = y.achievementID
+				 GROUP BY ID
+				 ORDER BY x.achievementID DESC) query1 
+			LEFT OUTER JOIN
+				(SELECT ID, User_B, GameTitle, SUM(PointValue) AS User_B_Points	--query2
+				FROM
+					(SELECT ID, User_B, GameTitle, achievementID 
+					 FROM
+						(SELECT ID, UserName AS User_B, name As GameTitle
+						FROM UserToGame u, Game g
+						WHERE u.UserName = frndusrname AND g.gameid = u.gameid) ug
+					 LEFT OUTER JOIN
+						AchievementToUserToGame atug
+					 ON ug.ID = atug.userToGameID) x
+				  LEFT OUTER JOIN 
+					Achievement y
+				ON x.achievementID = y.achievementID
+				GROUP BY ID
+				ORDER BY x.achievementID DESC) query2
+			ON query1.GameTitle = query2.GameTitle 
+			
+		UNION
+		
+		SELECT query2.GameTitle, User_A, User_A_Points, User_B, User_B_Points
+		FROM
+				(SELECT ID, User_A, GameTitle, SUM(PointValue) AS User_A_Points	--query1
+				 FROM
+					(SELECT ID, User_A, GameTitle, achievementID 
+					 FROM
+						(SELECT ID, UserName AS User_A, name As GameTitle
+						FROM UserToGame u, Game g
+						WHERE u.UserName = usrname AND g.gameid = u.gameid) ug
+					 LEFT OUTER JOIN
+						AchievementToUserToGame atug
+					 ON ug.ID = atug.userToGameID) x
+				  LEFT OUTER JOIN 
+					Achievement y
+				 ON x.achievementID = y.achievementID
+				 GROUP BY ID
+				 ORDER BY x.achievementID DESC) query1 
+			RIGHT OUTER JOIN
+				(SELECT ID, User_B, GameTitle, SUM(PointValue) AS User_B_Points	--query2
+				FROM
+					(SELECT ID, User_B, GameTitle, achievementID 
+					 FROM
+						(SELECT ID, UserName AS User_B, name As GameTitle
+						FROM UserToGame u, Game g
+						WHERE u.UserName = frndusrname AND g.gameid = u.gameid) ug
+					 LEFT OUTER JOIN
+						AchievementToUserToGame atug
+					 ON ug.ID = atug.userToGameID) x
+				  LEFT OUTER JOIN 
+					Achievement y
+				ON x.achievementID = y.achievementID
+				GROUP BY ID
+				ORDER BY x.achievementID DESC) query2
+			ON query1.GameTitle = query2.GameTitle;
+	/*Cursor query end*/
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+	/*Build statement for the temporary create comparison table screen
+	with procedure input variables in column/attribute names */
+	SET @userAchPoints = CONCAT('CREATE TABLE Compare_List (Game_Title VARCHAR(30), ', 
+					 'Your_Achievement_Points INT, ',
+					 'Achievement_Points_of_', frndusrname, ' INT, notOwned INT)');
+	/* Create user/friend game and achievements comparison table */
+	PREPARE stmnt FROM @userAchPoints;
+	EXECUTE stmnt;
+	DEALLOCATE PREPARE stmnt;
+	/* Populate Compare_Screen temporary output table: */
+	OPEN cur;
+	pop_loop: LOOP
+		FETCH cur INTO ttl, usrA, usrpointsA, usrB, usrpointsB;
+	    	IF done THEN
+			LEAVE pop_loop;
+	   	END IF;
+	   	/* Check if game owned by both users */
+	   	IF ((usrA IS NOT NULL) AND (usrB IS NOT NULL)) THEN
+	   		SET @owned = 1; --Set flag for sort order (1 = owned by both users)
+			/* Check if a user has null points for an owned game and set to 0 */
+			IF (usrpointsA IS NULL) THEN
+				SET usrpointsA = 0;
+			ELSEIF (usrpointsB IS NULL) THEN
+				SET usrpointsB = 0;
+			END IF;
+		ELSE SET @owned = 0; /* Set flag for sort order (0 = not owned by one user) */
+	   	END IF;
+	   	
+	   	INSERT INTO Compare_List 
+	   		VALUES (ttl, usrpointsA, usrpointsB, @owned);
+	END LOOP;
+	CLOSE cur;
+	/* Display result query string */
+	SET @resultStr = CONCAT('SELECT Game_Title, Your_Achievement_Points, Achievement_Points_of_',
+						frndusrname, ' FROM Compare_List ORDER BY notOwned DESC');
+	PREPARE stmnt FROM @resultStr;
+	EXECUTE stmnt;
+	DEALLOCATE PREPARE stmnt;
+	DROP TABLE Compare_List;
+END //
+DELIMITER ;
+
 
 /*
 QUESTION 17:
@@ -951,22 +1091,6 @@ BEGIN
 END; $$
 DELIMITER ;
 
-/*
-UserPublic triggers
-*/
-DROP TRIGGER IF EXISTS userNameEntryCheck;
-DELIMITER $$
-CREATE TRIGGER userNameEntryCheck
-BEFORE INSERT ON UserPublic
-FOR EACH ROW
-BEGIN
-	SET @usrname = NEW.userName;
-	SET @obscene = isUserNameRude(@usrname);
-	IF @obscene THEN
-		SET NEW.AccountStatus := 'Locked';
-	END IF;
-END; $$
-DELIMITER ;
 
 /* Question 20 ################################################# */
 
@@ -1178,7 +1302,7 @@ END $$
 DELIMITER ;
 
 /* Setup the TRIGGER on User-Public table*/
-DROP TRIGGER userNameEntryCheck;
+DROP TRIGGER IF EXISTS userNameEntryCheck;
 DELIMITER $$
 CREATE TRIGGER userNameEntryCheck
 BEFORE INSERT ON UserPublic
